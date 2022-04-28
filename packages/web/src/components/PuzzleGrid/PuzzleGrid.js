ee339e7e12654487321d39e137b2b997c965ae27
Christopher Liu <christopherliu830@gmail.com>
2022-04-27 22:51:57 -0700

    header was wrong on bigger boards

--- packages/web/src/components/PuzzleGrid/PuzzleGrid.js
+++ packages/web/src/components/PuzzleGrid/PuzzleGrid.js
@@ -38,7 +38,7 @@ export function PuzzleGrid({ puzzle, label}) {
     <Table w="220px" border="4px solid" borderColor="black" tableLayout="fixed">
       <Thead>
         <Tr>
-          <Th colSpan="5" textAlign="center" borderBottom="2px solid" borderColor="black">
+          <Th colSpan={width} textAlign="center" borderBottom="2px solid" borderColor="black">
             <Text position="relative" fontSize="2xl">
               {label} - {puzzle.calcs.secondsSpentSolving}
             </Text>
@@ -50,7 +50,7 @@ export function PuzzleGrid({ puzzle, label}) {
           <Tr key={idx}>
             {row.map((cell, idx) => (
               <Cell key={idx} bg={getColor(cell.timestamp)} w={`${(100/width)}%`}>
-                <Text position="relative" fontSize="3xl">
+                <Text position="relative" fontSize="2xl">
                   {cell.guess}
                 </Text>
               </Cell>

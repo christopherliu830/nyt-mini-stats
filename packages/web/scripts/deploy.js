90b2242f3c2aae547a65246e89da86897ba39526
Christopher Liu <christopherliu830@gmail.com>
2022-04-29 13:14:50 -0700

    pipe deploy script to stdio

--- packages/web/scripts/deploy.js
+++ packages/web/scripts/deploy.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const execSync = require('child_process').execSync;
-const output = execSync(`scp -r build/* ${process.env.DEPLOY_DIRECTORY}`, { encoding: 'utf-8' });
+const output = execSync(`scp -r build/* ${process.env.DEPLOY_DIRECTORY}`, { encoding: 'utf-8', stdio: 'inherit' });
 console.log(output);
 

575f040bbbd34b7464252ec045f636ed81e5735a
Christopher Liu <christopherliu830@gmail.com>
2022-04-29 11:31:15 -0700

    deploy script for web

---
+++ packages/web/scripts/deploy.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const execSync = require('child_process').execSync;
+const output = execSync(`scp -r build/* ${process.env.DEPLOY_DIRECTORY}`, { encoding: 'utf-8' });
+console.log(output);
+

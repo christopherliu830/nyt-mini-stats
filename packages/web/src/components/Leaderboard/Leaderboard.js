18dce59af02761e79f5202adc4bb12994274a4d6
Christopher Liu <christopherliu830@gmail.com>
2022-04-29 13:07:09 -0700

    leaderboard style changes

--- packages/web/src/components/Leaderboard/Leaderboard.js
+++ packages/web/src/components/Leaderboard/Leaderboard.js
@@ -1,12 +1,22 @@
-import { Table, Tbody, Th, Thead, Tr, Td } from "@chakra-ui/react";
+import { Box, Table, Tbody, Th, Thead, Tr, Td } from '@chakra-ui/react';
 
 export function Leaderboard({ stats, ...rest }) {
-  const placements = [...stats].sort((a, b) => (
-    a.puzzle?.calcs.secondsSpentSolving - b.puzzle?.calcs.secondsSpentSolving
-  ));
+  const placements = [...stats].sort(
+    (a, b) => a.puzzle?.calcs.secondsSpentSolving - b.puzzle?.calcs.secondsSpentSolving
+  );
 
   return (
-    <Table {...rest} w="400px">
+    <Table
+      {...rest}
+      borderRadius={8}
+      outline="solid 1px"
+      outlineColor="gray.100"
+      shadow="md"
+      overflow="hidden"
+      position="relative"
+      mt={4}
+      mb={4}
+    >
       <Thead>
         <Tr>
           <Th>Name</Th>
@@ -14,14 +24,13 @@ export function Leaderboard({ stats, ...rest }) {
         </Tr>
       </Thead>
       <Tbody>
-        { placements.map(({ user, puzzle }) => (
+        {placements.map(({ user, puzzle }) => (
           <Tr key={user}>
-            <Td>{user}</Td>
+            <Td fontWeight={600}>{user}</Td>
             <Td>{puzzle?.calcs.secondsSpentSolving ?? 'Not Done'}</Td>
           </Tr>
-
         ))}
       </Tbody>
     </Table>
-  )
+  );
 }
